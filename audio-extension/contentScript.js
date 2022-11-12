eb9f52061ef2de8885ff10ee1481e9ebeedb7655
Elon <elonmusk@gmail.com>
2022-11-12 16:30:19 -0600

    a

--- audio-extension/contentScript.js
+++
@@ -1,8 +0,0 @@
-(() => {
-    let test;
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, value, videoId } = obj;
-        console.log(videoId);
-        console.log(test);
-    });
-})();
