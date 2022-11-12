7287d74b1b58166d61e1c18615713371ecf0bbff
Min-Sung Choi <mchoi@Min-Sungs-MacBook-Air.local>
2022-11-12 16:35:31 -0600

    pulsating files

---
+++ reactapp/src/components/Loading.js
@@ -0,0 +1,10 @@
+import Pulsating from './pulsating';
+function Loading() {
+  return (
+    <Pulsating visible={true} width={120} height = {20}>
+        Loading...
+    </Pulsating>
+  );
+}
+
+export default Loading;
