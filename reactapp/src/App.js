8ac7d51077328ee01419ce2ade3bf60399e0417c
Elon <elonmusk@gmail.com>
2022-11-12 19:44:05 -0600

    hi

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,13 +3,11 @@
 
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
 
-=======
 import Loading from './components/Loading';
 import Beginning from './components/Beginning';
->>>>>>> 80a2cb2a2235da23fd56a3cc7e992ffa59586e5f
+import Main from './components/Main'
 function App() {
   const [response,setResponse] = useState("");
 
@@ -28,23 +26,9 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-     
-          Learn React
-          <button onClick={sendTestMessage}>test</button>
-          {response}
-
-      </header>
-=======
       <Main></Main>
       <Loading></Loading>
       <Beginning></Beginning>
->>>>>>> 80a2cb2a2235da23fd56a3cc7e992ffa59586e5f
     </div>
   );
 }
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-/*global chrome*/
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-     <App />
-);
-
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -23,4 +10,3 @@ root.render(
         <App />
     </ChakraProvider>
 );
->>>>>>> 80a2cb2a2235da23fd56a3cc7e992ffa59586e5f
