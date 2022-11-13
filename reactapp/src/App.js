4bd5a2548bcae3bd7e3c0796efbb1c9916bf417d
Elon <elonmusk@gmail.com>
2022-11-12 23:29:38 -0600

    hello

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,35 +3,42 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { useEffect, useState } from "react";
+import io from 'socket.io-client';
 
 import Loading from "./components/Loading";
 import Beginning from "./components/Beginning";
 import Main from "./components/Main";
 function App() {
-    const [response, setResponse] = useState("");
-
-    function sendTestMessage() {
+  
+    var socket = io("https://cheesehacks-backend.herokuapp.com/");
+    function openConnection() {
         chrome.tabs.query(
-            { active: true, currentWindow: true },
+            { active: true, currentWindow : true },
             function (tabs) {
-                console.log(tabs);
-                chrome.tabs.sendMessage(
-                    tabs[0].id,
-                    { greeting: "hello" },
-                    function (response) {
-                        console.log(response);
-                        setResponse(response);
-                    }
-                );
+              console.log(tabs);
+              let port =  chrome.tabs.connect(tabs[0].id, {name:"stream"});
+              chrome.tabCapture.getMediaStreamId({consumerTabId : tabs[0].id} , (capid)=>{
+                port.postMessage({command: "capture", streamId: capid});
+              })
+              port.onMessage.addListener((msg) => {
+                 
+                  //setSocket(io("http://localhost/"));
+                  console.log(msg);
+                  socket.send(msg);
+              });
+          
             }
         );
     }
 
     return (
         <div className="App">
+
+                      <button onClick={openConnection}>HELLO</button>
+{/* 
             <Main></Main>
             <Loading></Loading>
-            <Beginning></Beginning>
+            <Beginning></Beginning> */}
         </div>
     );
 }
